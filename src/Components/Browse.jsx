1e1050f7dfcbfe3f2c45a822e5655956382a9fb1
RuthvikKoutika7 <maniruthk7@gmail.com>
2025-03-29 22:38:08 +0530

    store setup and navigation to browse page

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,6 +1,9 @@
+import Header from "./Header";
+
 const Browse = () => {
   return (
-    <div>
+    <div className="flex">
+      <Header />
       <h1 className=" text-8xl text-red-700 ">Browse</h1>
     </div>
   );
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,6 +2,12 @@ import { useRef, useState } from "react";
 import { LOGIN_BG_IMG } from "../Utils/Constants";
 import Header from "./Header";
 import { checkDataIsValid } from "../Utils/Validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../Utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -9,15 +15,62 @@ const Login = () => {
   const fullName = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
+  const navigate = useNavigate();
 
   const toggleIsSignIn = () => {
     setIsSignInForm(!isSignInForm);
   };
 
-  const handleButtonClick =()=>{
-    const message = checkDataIsValid( email.current.value , password.current.value);
+  const handleButtonClick = () => {
+    const message = checkDataIsValid(
+      email.current.value,
+      password.current.value
+    );
     setErrorMessage(message);
-  }
+    if (message) return;
+
+    if (!isSignInForm) {
+      // Sign Up Logic
+
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " - " + errorMessage);
+          // ..
+        });
+    } else {
+      //Sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " - " + errorMessage);
+        });
+    }
+  };
 
   return (
     <div>
@@ -26,7 +79,10 @@ const Login = () => {
         <img alt="bg-img" src={LOGIN_BG_IMG} className="w-full" />
       </div>
 
-      <form onSubmit={(e)=> e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-xl bg-">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-xl "
+      >
         <h1 className="text-4xl font-bold py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -51,7 +107,10 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700 "
         />
         <h1 className="text-red-500 font-bold text-lg">{errorMessage}</h1>
-        <button className="p-4 my-6 w-full cursor-pointer bg-red-600 rounded-xl" onClick={handleButtonClick}>
+        <button
+          className="p-4 my-6 w-full cursor-pointer bg-red-600 rounded-xl"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-6" onClick={toggleIsSignIn}>
