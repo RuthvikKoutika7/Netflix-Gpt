658b5c52b0b3b8ac8a4c9720b855dbbfa59f8e82
Ruthvik Koutika <67162268+RuthvikKoutika7@users.noreply.github.com>
2025-04-10 13:41:11 +0530

    Update MainContainer.jsx

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -4,13 +4,8 @@ import VideoTitle from "./VideoTitle";
 
 const MainContainer = ()=>{
     const movies = useSelector(store => store.movies?.nowPlayingMovies);
-    if(!movies) return null;
-
-    
+    if(!movies) return null;  
     const {original_title, overview, id} = movies[11];
-    
-
-
     return(
         <div>
             <VideoTitle title={original_title} overview={overview} />
